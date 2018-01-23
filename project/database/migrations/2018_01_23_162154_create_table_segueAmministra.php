5de3d797ccbd3edca829dc1424c84bc76aa9ffd2
Damiano <damiano.azzolini@student.unife.it>
2018-01-23 17:29:23 +0100

    Aggiornate migrations

--- project/database/migrations/2018_01_23_162154_create_table_segueAmministra.php
+++ project/database/migrations/2018_01_23_162154_create_table_segueAmministra.php
@@ -4,21 +4,19 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTablePost extends Migration {
+class CreateTableSegueAmministra extends Migration {
     public function up() {
-        Schema::create('post',function (Blueprint $table) {
-            $table->increments('postID');
+        Schema::create('segueAmministra',function (Blueprint $table) {
             $table->timestamps();
             $table->integer('utenteID')->unsigned();
             $table->foreign('utenteID')->references('utenteID')->on('utente');
-            $table->text('contenuto')->nullable();
-            $table->boolean('attivo'); //0 non attivo, 1 attivo
-            $table->integer('paginaID')->unsigned()->nullable();
+            $table->integer('paginaID')->unsigned();
             $table->foreign('paginaID')->references('paginaID')->on('pagina');
+            $table->string('stato');
         });
     }
 
     public function down() {
-        Schema::dropIfExists('post');
+        Schema::dropIfExists('segueAmministra');
     }
 }
